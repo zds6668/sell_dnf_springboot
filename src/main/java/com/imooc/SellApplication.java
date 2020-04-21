cf456da0aa88ebca392b2f0f775f111703d084ea
jdk8_666 <yjdqw@126.com>
2020-04-22 02:02:41 +0800

    mybatis使用与测试

--- src/main/java/com/imooc/SellApplication.java
+++ src/main/java/com/imooc/SellApplication.java
@@ -1,9 +1,11 @@
 package com.imooc;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.imooc.dataobject.mapper")
 public class SellApplication {
 
     public static void main(String[] args) {
